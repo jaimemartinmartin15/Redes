99b7b99bbade74a09ba7dc6f1b2bae2ff43b041f
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2024-06-15 20:34:16 +0200

    Fix link and full image directive.

--- src/app/dns/configure-dns-server/configure-dns-server.component.ts
+++ src/app/dns/configure-dns-server/configure-dns-server.component.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { HeaderPrintComponent, ResponsiveLayoutComponent, TerminalCodeComponent } from '@jaimemartinmartin15/jei-devkit-angular-shared';
+import {
+  HeaderPrintComponent,
+  ImageFullScreenModule,
+  ResponsiveLayoutComponent,
+  TerminalCodeComponent,
+} from '@jaimemartinmartin15/jei-devkit-angular-shared';
 
 @Component({
   selector: 'app-configure-dns-server',
   standalone: true,
-  imports: [CommonModule, RouterLink, ResponsiveLayoutComponent, HeaderPrintComponent, TerminalCodeComponent],
+  imports: [CommonModule, RouterLink, ResponsiveLayoutComponent, HeaderPrintComponent, TerminalCodeComponent, ImageFullScreenModule],
   templateUrl: './configure-dns-server.component.html',
   styleUrls: ['./configure-dns-server.component.scss'],
 })
